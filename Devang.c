78c835c56f1c905453f42c686fb9da0769d1ceea
devang014 <mrdevangsingh@gmail.com>
2018-06-10 01:59:50 +0530

    my second commit

--- Devang.c
+++ Devang.c
@@ -1,7 +1,7 @@
 #include<stdio.h>//program to calculae value of  number raised to any power
 int main(){
 int y=1,x,i,n;
-printf("enter the number\n");
+printf("enter the number\n");//tatti
 scanf("%d",&x);
 for(i=1;i<=n;i++){
 y=y*x;}
