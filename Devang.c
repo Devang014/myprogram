133742a16b2269033d126447cfc9bf69f2b18703
devang014 <mrdevangsingh@gmail.com>
2018-06-10 01:55:31 +0530

    my first commit

---
+++ Devang.c
@@ -0,0 +1,10 @@
+#include<stdio.h>//program to calculae value of  number raised to any power
+int main(){
+int y=1,x,i,n;
+printf("enter the number\n");
+scanf("%d",&x);
+for(i=1;i<=n;i++){
+y=y*x;}
+printf("%d",y);
+return 0;
+}
